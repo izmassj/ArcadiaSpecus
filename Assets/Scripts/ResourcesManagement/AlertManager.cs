43b2d14f529902af285801fea17c90140db640f0
AlexRodBlasIns <a23alerodbla@inspedralbes.cat>
2025-11-24 17:15:11 +0100

    Sistema de recursos y NPCs funcional. UI de ambas cosas.

---
+++ Assets/Scripts/ResourcesManagement/AlertManager.cs
@@ -0,0 +1,30 @@
+﻿// AlertManager.cs
+using UnityEngine;
+
+public class AlertManager : MonoBehaviour
+{
+    public static AlertManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        // Suscribirse al evento de recursos críticos
+        ResourceManager.OnResourceCritical += OnResourceCritical;
+    }
+
+    /// <summary>
+    /// Maneja el evento cuando un recurso alcanza nivel crítico
+    /// </summary>
+    /// <param name="resourceType">Tipo de recurso en estado crítico</param>
+    void OnResourceCritical(ResourceType resourceType)
+    {
+        Debug.Log($"ALERTA: {resourceType} está en nivel crítico!");
+    }
+}
