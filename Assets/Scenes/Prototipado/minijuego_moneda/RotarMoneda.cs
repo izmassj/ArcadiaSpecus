6e3a6468267461c0990e191c1c0d769eb4bd5c0c
PC_MIGUEL\migue <miguelcerver05@gmail.com>
2026-05-11 09:34:26 +0200

    Script creado con la lógica del minijuego de la moneda

--- Assets/Scenes/Prototipado/minijuego_moneda/RotarMoneda.cs
+++ Assets/Scenes/Prototipado/minijuego_moneda/RotarMoneda.cs
@@ -1,11 +1,96 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RotarMoneda : MonoBehaviour
 {
-    public float velocidad = 100f;
+    [Header("Moneda")]
+    public Transform moneda;
+    public float velocidadRotacion = 800f;
+
+    [Header("UI")]
+    public TMP_Text textoTokens;
+    public TMP_Text resultadoTexto;
+
+    private int tokens = 500;
+    private int apuesta = 50;
+
+    private bool girando = false;
+
+    void Start()
+    {
+        ActualizarUI();
+    }
 
     void Update()
     {
-        transform.Rotate(velocidad * Time.deltaTime, 0f, 0f);
+        if (girando)
+        {
+            moneda.Rotate(Vector3.right * velocidadRotacion * Time.deltaTime);
+        }
+    }
+
+    // BOTONES DE APUESTA
+    public void Apostar50()
+    {
+        apuesta = 50;
+    }
+
+    public void Apostar100()
+    {
+        apuesta = 100;
+    }
+
+    public void Apostar200()
+    {
+        apuesta = 200;
+    }
+
+    // BOTON PLAY
+    public void Jugar()
+    {
+        if (girando) return;
+
+        if (tokens < apuesta)
+        {
+            resultadoTexto.text = "No tienes suficientes tokens";
+            return;
+        }
+
+        tokens -= apuesta;
+        ActualizarUI();
+
+        StartCoroutine(TirarMoneda());
+    }
+
+    System.Collections.IEnumerator TirarMoneda()
+    {
+        girando = true;
+
+        yield return new WaitForSeconds(2f);
+
+        girando = false;
+
+        
+        int resultado = Random.Range(0, 2);
+
+        if (resultado == 1)
+        {
+            int premio = apuesta * 2;
+            tokens += premio;
+
+            resultadoTexto.text = "¡GANASTE! +" + premio;
+        }
+        else
+        {
+            resultadoTexto.text = "Perdiste";
+        }
+
+        ActualizarUI();
+    }
+
+    void ActualizarUI()
+    {
+        textoTokens.text = "Tokens: " + tokens;
     }
 }
